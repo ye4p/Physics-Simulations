75780be4ca2f7d837ae3b0aabd425e58822bbd99
ye4p <danilgolubkov172@gmail.com>
2026-07-30 18:11:47 -0400

    fix

---
+++ build/CMakeFiles/physics_engine.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/danil/OneDrive/Desktop/Programming/repos/physics_engine/src/display.cpp" "CMakeFiles/physics_engine.dir/src/display.cpp.obj" "gcc" "CMakeFiles/physics_engine.dir/src/display.cpp.obj.d"
+  "C:/Users/danil/OneDrive/Desktop/Programming/repos/physics_engine/src/main.cpp" "CMakeFiles/physics_engine.dir/src/main.cpp.obj" "gcc" "CMakeFiles/physics_engine.dir/src/main.cpp.obj.d"
+  "C:/Users/danil/OneDrive/Desktop/Programming/repos/physics_engine/src/physics.cpp" "CMakeFiles/physics_engine.dir/src/physics.cpp.obj" "gcc" "CMakeFiles/physics_engine.dir/src/physics.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/physics_engine.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/physics_engine.dir/src/display.cpp.obj"
+  "CMakeFiles/physics_engine.dir/src/display.cpp.obj.d"
+  "CMakeFiles/physics_engine.dir/src/main.cpp.obj"
+  "CMakeFiles/physics_engine.dir/src/main.cpp.obj.d"
+  "CMakeFiles/physics_engine.dir/src/physics.cpp.obj"
+  "CMakeFiles/physics_engine.dir/src/physics.cpp.obj.d"
+  "libphysics_engine.dll.a"
+  "physics_engine.exe"
+  "physics_engine.exe.manifest"
+  "physics_engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/physics_engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
